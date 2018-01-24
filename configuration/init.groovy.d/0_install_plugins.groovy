caebddaf72bdf8bbe618a5c558d9b4a2d83d7fb8
qxu1689 <Christopher.Troeger@iteratec.de>
2018-01-24 16:22:00 +0100

    Adaptions for s2i build

--- configuration/init.groovy.d/0_install_plugins.groovy
+++ configuration/init.groovy.d/0_install_plugins.groovy
@@ -24,36 +24,36 @@ if (env["USE_PROXY"] != null && env["USE_PROXY"] == "true") {
     instance.save()
 }
 
-// Install plugins
-println("Installing plugins..")
-def updateCenter = instance.updateCenter
-updateCenter.updateAllSites()
-def queue = new LinkedBlockingQueue<Future<UpdateCenter>>()
-//FIXME use specific plugin versions
-//maven-plugin:3.0
-//nodejs:1.2.4
-//docker-plugin:1.0.4
-//kubernetes:1.1.3
-//openshift-sync:0.1.32
-//openshift-login:1.0.0
-//openshift-pipeline:1.0.52
-//config-file-provider:2.16.4
-//pipeline-npm:0.9.1
-queue.add(updateCenter.getPlugin("config-file-provider").deploy())
-queue.add(updateCenter.getPlugin("maven-plugin").deploy())
-queue.add(updateCenter.getPlugin("nodejs").deploy())
-queue.add(updateCenter.getPlugin("docker-plugin").deploy())
-queue.add(updateCenter.getPlugin("kubernetes").deploy())
-queue.add(updateCenter.getPlugin("openshift-sync").deploy())
-queue.add(updateCenter.getPlugin("openshift-login").deploy())
-queue.add(updateCenter.getPlugin("openshift-pipeline").deploy())
-queue.add(updateCenter.getPlugin("openshift-client").deploy())
-queue.add(updateCenter.getPlugin("pipeline-npm").deploy())
-queue.collect { it.get() }
-if (updateCenter.isRestartRequiredForCompletion()) {
-    println("Restart required")
-    Jenkins.instance.restart()
-    throw new RestartRequiredException(null)
-} else {
-    println("Installing plugins done")
-}
+//// Install plugins
+//println("Installing plugins..")
+//def updateCenter = instance.updateCenter
+//updateCenter.updateAllSites()
+//def queue = new LinkedBlockingQueue<Future<UpdateCenter>>()
+////FIXME use specific plugin versions
+////maven-plugin:3.0
+////nodejs:1.2.4
+////docker-plugin:1.0.4
+////kubernetes:1.1.3
+////openshift-sync:0.1.32
+////openshift-login:1.0.0
+////openshift-pipeline:1.0.52
+////config-file-provider:2.16.4
+////pipeline-npm:0.9.1
+//queue.add(updateCenter.getPlugin("config-file-provider").deploy())
+//queue.add(updateCenter.getPlugin("maven-plugin").deploy())
+//queue.add(updateCenter.getPlugin("nodejs").deploy())
+//queue.add(updateCenter.getPlugin("docker-plugin").deploy())
+//queue.add(updateCenter.getPlugin("kubernetes").deploy())
+//queue.add(updateCenter.getPlugin("openshift-sync").deploy())
+//queue.add(updateCenter.getPlugin("openshift-login").deploy())
+//queue.add(updateCenter.getPlugin("openshift-pipeline").deploy())
+//queue.add(updateCenter.getPlugin("openshift-client").deploy())
+//queue.add(updateCenter.getPlugin("pipeline-npm").deploy())
+//queue.collect { it.get() }
+//if (updateCenter.isRestartRequiredForCompletion()) {
+//    println("Restart required")
+//    Jenkins.instance.restart()
+//    throw new RestartRequiredException(null)
+//} else {
+//    println("Installing plugins done")
+//}
