3e2584017dd6aedd676a49fdbdaa1ac3299d0427
qxu1689 <Christopher.Troeger@iteratec.de>
2018-03-14 15:38:10 +0100

    Added setting "hudson.slaves.WorkspaceList" to "_" to avoid confusion in concurrent builds

--- configuration/init.groovy.d/5_configure-jenkins-itself.groovy
+++ configuration/init.groovy.d/5_configure-jenkins-itself.groovy
@@ -1,5 +1,6 @@
 import jenkins.model.Jenkins
 import hudson.slaves.EnvironmentVariablesNodeProperty
+import hudson.slaves.WorkspaceList
 
 instance = Jenkins.getInstance()
 
@@ -22,3 +23,8 @@ if (envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0) {
 }
 envVars.put("NO_PROXY", "nexus")
 instance.save()
+
+
+// Set "hudson.slaves.WorkspaceList" to "_" to avoid confusion in concurrent builds
+println("Adding \"hudson.slaves.WorkspaceList\" to \"_\"..")
+System.setProperty(WorkspaceList.class.getName(), "_");
