a59692fba4ff9c61933e55d7f2211ae808e9df3a
Vincent Flecke <Vincent.Flecke@iteratec.de>
2018-07-27 13:36:53 +0200

    Fix global config files not being created on INT and PROD

--- configuration/init.groovy.d/2_configure-managed-files.groovy
+++ configuration/init.groovy.d/2_configure-managed-files.groovy
@@ -3,54 +3,40 @@ import jenkins.plugins.nodejs.configfiles.NPMConfig
 import org.jenkinsci.plugins.configfiles.maven.*
 import org.jenkinsci.plugins.configfiles.maven.security.*
 
-def env = System.getenv()
-
 def store = Jenkins.instance.getExtensionList('org.jenkinsci.plugins.configfiles.GlobalConfigFiles')[0]
 
 // NPM Config
-if (env["TARGET_ENVIRONMENT"] == "TEST") {
-    println("Creating \"npm-config\"...")
-    String npmConfigContent = new File("/var/lib/jenkins/configFileTemplates/npm-config.txt").text
-    def npmConfig = new NPMConfig(
-            "npm-config",
-            "npmrc",
-            "npmrc",
-            npmConfigContent,
-            "",
-            null)
-    store.save(npmConfig)
-} else {
-    println("Skipping creation of \"npm-config\": Not in target environment.")
-}
+println("Creating \"npm-config\"...")
+String npmConfigContent = new File("/var/lib/jenkins/configFileTemplates/npm-config.txt").text
+def npmConfig = new NPMConfig(
+        "npm-config",
+        "npmrc",
+        "npmrc",
+        npmConfigContent,
+        "",
+        null)
+store.save(npmConfig)
 
 // NPM Nexus Config
-if (env["TARGET_ENVIRONMENT"] == "TEST") {
-    println("Creating \"npm-nexus-config\"...")
-    String npmNexusConfigContent = new File("/var/lib/jenkins/configFileTemplates/npm-nexus-config.txt").text
-    def npmNexusConfig = new NPMConfig(
-            "npm-nexus-config",
-            "NPM Nexus deployment config",
-            "NPM Nexus deployment config",
-            npmNexusConfigContent,
-            "",
-            null)
-    store.save(npmNexusConfig)
-} else {
-    println("Skipping creation of \"npm-nexus-config\": Not in target environment.")
-}
+println("Creating \"npm-nexus-config\"...")
+String npmNexusConfigContent = new File("/var/lib/jenkins/configFileTemplates/npm-nexus-config.txt").text
+def npmNexusConfig = new NPMConfig(
+        "npm-nexus-config",
+        "NPM Nexus deployment config",
+        "NPM Nexus deployment config",
+        npmNexusConfigContent,
+        "",
+        null)
+store.save(npmNexusConfig)
 
 // Maven Settings
-if (env["TARGET_ENVIRONMENT"] == "TEST") {
-    println("Creating \"maven-settings\"...")
-    String mavenSettingsContent = new File("/var/lib/jenkins/configFileTemplates/maven-settings.txt").text
-    def mavenSettings = new MavenSettingsConfig(
-            "maven-settings",
-            "Maven Settings",
-            "maven settings",
-            mavenSettingsContent,
-            false,
-            null)
-    store.save(mavenSettings)
-} else {
-    println("Skipping creation of \"maven-settings\": Not in target environment.")
-}
+println("Creating \"maven-settings\"...")
+String mavenSettingsContent = new File("/var/lib/jenkins/configFileTemplates/maven-settings.txt").text
+def mavenSettings = new MavenSettingsConfig(
+        "maven-settings",
+        "Maven Settings",
+        "maven settings",
+        mavenSettingsContent,
+        false,
+        null)
+store.save(mavenSettings)
